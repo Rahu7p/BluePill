fdc648b3b3798c7222e4e3943c1c1cae2430565c
Rahu7p <78507802+Rahu7p@users.noreply.github.com>
2022-06-01 23:52:35 -0500

    Update helloTask.c

--- FreeRTOS/helloTask.c
+++ FreeRTOS/helloTask.c
@@ -4,14 +4,8 @@
   * @file           : main.c
   * @brief          : Main program body
   ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
+  * This program creates one Task using the CMSIS_V1 API (FreeRTOS).
+  * The Task prints every one second using the UART protocol.
   *
   ******************************************************************************
   */
