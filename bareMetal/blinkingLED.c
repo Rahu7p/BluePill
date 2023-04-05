923a8750e419761f15b9c2309be7e0d86feb15c6
Rahu7p <78507802+Rahu7p@users.noreply.github.com>
2023-04-05 14:55:57 -0600

    Update blinkingLED.c

--- bareMetal/blinkingLED.c
+++ bareMetal/blinkingLED.c
@@ -95,10 +95,10 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  GPIOA->BSRR = GPIO_BSRR_BS5;//	LED ON
-	  HAL_Delay(1000);//				1 second delay
-	  GPIOA->BSRR = GPIO_BSRR_BR5;//	LED OFF
-	  HAL_Delay(1000);//				1 second delay
+	  GPIOA->BSRR = GPIO_BSRR_BS5;//	LD2 ON
+	  HAL_Delay(1000);//			1 second delay
+	  GPIOA->BSRR = GPIO_BSRR_BR5;//	LD2 OFF
+	  HAL_Delay(1000);//			1 second delay
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -177,10 +177,11 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 void USER_RCC_Init(void){
-	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;//	I/O port A clock enable
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;//			I/O port A clock enable
 }
+
 void USER_GPIO_Init(void){
-	GPIOA->BSRR = GPIO_BSRR_BR5;//			PA5 -> 1, LED OFF
+	GPIOA->BSRR = GPIO_BSRR_BR5;//				PA5 -> 1, LD2 OFF
 	//pin A5 as output push-pull max speed 10MHz
 	GPIOA->CRL &= ~GPIO_CRL_CNF5 & ~GPIO_CRL_MODE5_1;
 	GPIOA->CRL |= GPIO_CRL_MODE5_0;
